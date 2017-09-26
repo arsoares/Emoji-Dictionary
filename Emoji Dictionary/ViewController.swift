af37a578344523e25b43b013298f9e43e1d54d48
Austin Soares <arsoares@me.com>
2017-09-26 09:05:20 -0400

    Added tableView and Source and Delegate function calls

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var referenceView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        referenceView.dataSource = self
+        referenceView.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
